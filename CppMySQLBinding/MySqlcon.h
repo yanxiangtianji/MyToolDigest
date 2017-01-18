f8b47130237d14e8c9170105885c2a96617eca46
yxtj <yanxiangtianji@gmail.com>
2017-01-18 11:35:56 -0500

    modify MySQL connection class

--- CppMySQLBinding/MySqlcon.h
+++ CppMySQLBinding/MySqlcon.h
@@ -17,7 +17,7 @@ class MySQLcon
 using namespace std;
 /* typedef */
 public:
-	typedef vector<vector<string> > result_type;
+	typedef std::vector<std::vector<std::string> > result_type;
 /* data members */
 private:
 	_MySQLcon_inner *pimpl;
@@ -30,44 +30,44 @@ public:
 	MySQLcon& operator=(const MySQLcon&) = delete;
 	MySQLcon(MySQLcon&& ref);
 	MySQLcon& operator=(MySQLcon&& ref);
-	MySQLcon(const string& host, const string& dbname,const string& username,const string& password);
+	MySQLcon(const std::string& host, const std::string& dbname,const std::string& username,const std::string& password);
 	~MySQLcon();
 
 /* helper member functions */
 private:
-	void db_connect(const string& host,const string& db,const string& user,const string& pw);
+	void db_connect(const std::string& host,const std::string& db,const std::string& user,const std::string& pw);
 	void db_close();
 
 /* member functions */
 public:
-	string get_error();
-	string get_version();
+	std::string get_error();
+	std::string get_version();
 	unsigned long long affected_rows();
 
 	bool query(const char* str);//throws
 
-	result_type get_result(vector<string>* head=nullptr);	//if head!=nullptr; return the header line
-	void get_result_append(result_type& res,vector<string>* head=nullptr);
+	result_type get_result(std::vector<std::string>* head=nullptr);	//if head!=nullptr; return the header line
+	void get_result_append(result_type& res,std::vector<std::string>* head=nullptr);
 //	result_type get_result(ostream &fout,bool withhead);//output to stream
 
 	template<class T>
-	vector<T> get_result(function<T(const vector<string>&)> fn,vector<string>* head=nullptr);
+	std::vector<T> get_result(function<T(const std::vector<std::string>&)> fn,std::vector<std::string>* head=nullptr);
 	template<class T>
-	void get_result_append(vector<T>& res,function<T(const vector<string>&)> fn,vector<string>* head=nullptr);
+	void get_result_append(std::vector<T>& res,function<T(const std::vector<std::string>&)> fn,std::vector<std::string>* head=nullptr);
 	
-	void process_result(function<void(const vector<string>&)> fn, vector<string>* head = nullptr);
+	void process_result(function<void(const std::vector<std::string>&)> fn, std::vector<std::string>* head = nullptr);
 };
 
 template<class T>
-inline vector<T> MySQLcon::get_result(function<T(const vector<string>&)> fn,vector<string>* head){
-	vector<T> res;
+inline std::vector<T> MySQLcon::get_result(function<T(const std::vector<std::string>&)> fn,std::vector<std::string>* head){
+	std::vector<T> res;
 	get_result_append(res,fn, head);
 	return res;
 }
 
 template<class T>
-void MySQLcon::get_result_append(vector<T>& res,function<T(const vector<string>&)> fn,vector<string>* head){
-	process_result([&fn, &res](const vector<string>& line){
+void MySQLcon::get_result_append(std::vector<T>& res,function<T(const std::vector<std::string>&)> fn,std::vector<std::string>* head){
+	process_result([&fn, &res](const std::vector<std::string>& line){
 		res.push_back(fn(line));
 	}, head);
 }
